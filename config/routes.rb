39b64163b1c425cae6c6ad86bdbeca68041369ba
Andre Smith <andrepsmithjr@gmail.com>
2014-03-22 13:53:33 -0700

    Start Facebook login. WIP

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ VisionhouseRails::Application.routes.draw do
   resources :users
 
   get '/' , to: 'view_pages#index'
+
+  match 'auth/facebook/callback', to: 'sessions#create', as: 'signin', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322060136) do
+ActiveRecord::Schema.define(version: 20140322192548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140322060136) do
     t.string   "classification"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
 end
