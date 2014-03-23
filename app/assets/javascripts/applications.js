6c46da15e0676aa4e395b92b25b532ce1bb3645c
admin <lcdvirgo@gmail.com>
2014-03-22 23:05:05 -0700

    testing javascript loading

---
+++ app/assets/javascripts/applications.js
@@ -0,0 +1,16 @@
+/*Place all the behaviors and hooks related to the matching controller here.
+All this logic will automatically be available in application.js.
+You can use CoffeeScript in this file: http://coffeescript.org/
+Switch to applications.js.coffee if using CoffeeScript */
+
+$(document).ready( function() {
+    alert("Hello World!");
+	/* $( '#content' ).each(function () {
+    	this.style.setProperty( 'background', '#eff1f5', 'important' );
+	});
+    	$('#typedcomplete').delay(16000).fadeOut();
+	$("#tagline span").delay(17500).text("What's yours?").fadeIn();
+	$("#button span").delay(19500).text("Find out now").fadeIn(); */
+
+});
+
