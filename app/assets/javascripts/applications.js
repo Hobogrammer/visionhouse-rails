47028cb0a764ea40c726eae57899085605f28f0c
admin <lcdvirgo@gmail.com>
2014-03-23 06:05:57 -0700

    removed js

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -3,14 +3,4 @@ All this logic will automatically be available in application.js.
 You can use CoffeeScript in this file: http://coffeescript.org/
 Switch to applications.js.coffee if using CoffeeScript */
 
-$(document).ready( function() {
-    alert("Hello World!");
-	/* $( '#content' ).each(function () {
-    	this.style.setProperty( 'background', '#eff1f5', 'important' );
-	});
-    	$('#typedcomplete').delay(16000).fadeOut();
-	$("#tagline span").delay(17500).text("What's yours?").fadeIn();
-	$("#button span").delay(19500).text("Find out now").fadeIn(); */
-
-});
 
