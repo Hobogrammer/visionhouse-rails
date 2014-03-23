e972603339827bb9e65e537578ae06ad4e702ea6
Ramin <rsedighi@gmail.com>
2014-03-23 13:24:08 -0700

    Fixing form views

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,7 +7,7 @@ class ApplicationsController < ApplicationController
   end
 
   def show
-    @application = Application.includes(:children,:family_members,:criminal_arrests,:employments,:housing_histories,:people).find(params[:id])
+    @application = Application.includes(:children,:family_members,:criminal_arrests,:employments,:housing_histories,:people, :subject).find(params[:id])
   end
 
   def new
